68fc334195513421f58cca21bd87a7534dc7d9c1
Jason <chenjxlg@163.com>
2021-01-28 12:04:33 +0800

    挖矿接口

--- src/Chain33/Miner/Client.php
+++ src/Chain33/Miner/Client.php
@@ -13,9 +13,9 @@ class Client extends BaseClient
 
     /**
      * Notes   : 获取执行器地址
-     * @Author : <C.Jason>
-     * @Date   : 2020/4/30 22:21
-     * @param  string  $name
+     * @Date   : 2021/1/28 11:58 上午
+     * @Author : < Jason.C >
+     * @param  string  $name  执行器名称
      * @return string
      */
     public function execer(string $name): string
@@ -26,9 +26,9 @@ class Client extends BaseClient
     }
 
     /**
-     * Notes: 绑定挖矿地址
-     * @Author: <C.Jason>
-     * @Date  : 2020/4/30 22:24
+     * Notes   : 绑定挖矿地址
+     * @Author : <C.Jason>
+     * @Date   : 2020/4/30 22:24
      * @param  string  $bindAddr      挖矿绑定地址
      * @param  string  $originAddr    原始地址
      * @param  int     $amount        用于购买ticket的bty数量
@@ -46,9 +46,9 @@ class Client extends BaseClient
     }
 
     /**
-     * Notes: 设置自动挖矿
-     * @Author: <C.Jason>
-     * @Date  : 2020/4/30 22:26
+     * Notes   : 设置自动挖矿
+     * @Author : <C.Jason>
+     * @Date   : 2020/4/30 22:26
      * @param  int  $flag  标识符，1 为开启自动挖矿，0 为关闭自动挖矿
      * @return bool
      */
@@ -60,9 +60,9 @@ class Client extends BaseClient
     }
 
     /**
-     * Notes: 获取Ticket的数量
-     * @Author: <C.Jason>
-     * @Date  : 2020/4/30 22:29
+     * Notes   : 获取Ticket的数量
+     * @Author : <C.Jason>
+     * @Date   : 2020/4/30 22:29
      * @return int
      */
     public function ticket(): int
@@ -71,17 +71,16 @@ class Client extends BaseClient
     }
 
     /**
-     * Notes: 获取绑定的挖矿地址
-     * @Author: <C.Jason>
-     * @Date  : 2020/4/30 22:32
-     * @param  string  $execer  执行器名称
-     * @param  string  $addr    冷钱包地址
-     * @return int
+     * Notes   : 获取绑定的挖矿地址
+     * @Author : <C.Jason>
+     * @Date   : 2020/4/30 22:32
+     * @param  string  $addr  冷钱包地址
+     * @return string         返回挖矿地址
      */
-    public function addr(string $execer, string $addr): int
+    public function addr(string $addr): string
     {
         return $this->client->Query([
-            'execer'   => $execer,
+            'execer'   => 'ticket',
             'funcName' => 'MinerAddress',
             'payload'  => [
                 'data' => $addr,
@@ -90,17 +89,16 @@ class Client extends BaseClient
     }
 
     /**
-     * Notes: 获取矿工地址的对应的冷钱包地址
-     * @Author: <C.Jason>
-     * @Date  : 2020/4/30 22:35
-     * @param  string  $execer  执行器名称
-     * @param  string  $addr    矿工地址
-     * @return array
+     * Notes   : 获取矿工地址的对应的冷钱包地址
+     * @Author : <C.Jason>
+     * @Date   : 2020/4/30 22:35
+     * @param  string  $addr  矿工地址
+     * @return array          冷钱包地址列表
      */
-    public function source(string $execer, string $addr): array
+    public function source(string $addr): array
     {
         return $this->client->Query([
-            'execer'   => $execer,
+            'execer'   => 'ticket',
             'funcName' => 'MinerSourceList',
             'payload'  => [
                 'data' => $addr,
@@ -109,10 +107,10 @@ class Client extends BaseClient
     }
 
     /**
-     * Notes: 关闭指定账户绑定的
-     * 可以填空参数close所有当前钱包账户绑定的ticket
-     * @Author: <C.Jason>
-     * @Date  : 2020/4/30 22:37
+     * Notes   : 关闭指定账户绑定的 tickets
+     *           可以填空参数close所有当前钱包账户绑定的ticket
+     * @Author : <C.Jason>
+     * @Date   : 2020/4/30 22:37
      * @param  string  $minerAddress
      * @return array
      */
@@ -124,9 +122,9 @@ class Client extends BaseClient
     }
 
     /**
-     * Notes: 构造买票交易
-     * @Author: <C.Jason>
-     * @Date  : 2020/4/30 22:42
+     * Notes   : 构造买票交易
+     * @Author : <C.Jason>
+     * @Date   : 2020/4/30 22:42
      * @param  string  $minerAddress   挖矿地址
      * @param  string  $returnAddress  收益地址，(非委托挖矿时，和挖矿地址一致)
      * @param  int     $count          买票数量
